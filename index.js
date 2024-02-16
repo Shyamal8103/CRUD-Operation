07ade248657b3080fb8ec503463e40047d6b4cf0
Shyamal <shivakumarsk5308@gmail.com>
2024-02-17 02:09:46 +0530

    added

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.patch('/product/:id',productsController.UpdateProduct)
 app.delete('/product/:id',productsController.DeleteProduct)
 
 app.listen(process.env.PORT,()=>{
-    console.log(`app is listen in port 8000`)
+    console.log(`app is listen in port ${process.env.port}`)
 })
 
 // console.log('hello')
